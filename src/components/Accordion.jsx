c24cbc6885f4cc97df0ec651172c565604274845
ABQ <obeitoraq20@gmail.com>
2024-04-01 22:35:21 +0100

    accordion functionality implemented

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Accordion = () => {
+    const [accordionOpen, setAccordionOpen] = useState(false);
     return(
         <>
-            <p>Accordion</p>
+        <div className=" text-[#000000AD]">
+            <button 
+                onClick={() => setAccordionOpen(!accordionOpen)}
+                className="flex justify-between w-full"
+            >
+                <span className="font-bold">This is the question</span>
+                {accordionOpen ? <span>-</span> : <span>+</span>}
+            </button>
+            <div className={`grid overflow-hidden transition-all duration-300 ease-in-out ${
+                accordionOpen 
+                ? 'grid-rows-[1fr] opacity-100 py-4'
+                : 'grid-rows-[0fr] opacity-0'
+            }`}>
+                <div className="overflow-hidden">This is the answer</div>
+            </div>
+        </div>
+            
         </>
     )
 }
--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -5,8 +5,8 @@ const FAQ = () => {
     return(
         <>
         <div className="w-[747px] mx-auto space-y-6">
-            <h2 className="font-bold text-center">FAQ</h2>
-            <p className="text-center">Some frequently asked questions at the academy</p>
+            <h2 className="font-bold text-center text-5xl text-[#182233]">FAQ</h2>
+            <p className="text-center text-lg font-normal">Some frequently asked questions at the academy</p>
             <div className="p-4 rounded-lg bg-[#F8F9F9]">
                 <Accordion />
             </div>
