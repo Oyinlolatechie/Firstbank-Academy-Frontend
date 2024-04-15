86bd2d23a27bd298665c78c3dce68b7ebe80c2a6
Oyinlolatechie <adepitanoyinlola4@gmail.com>
2024-04-15 12:47:50 +0100

    feat: implementing the navbar mobile view: hammbugger icon added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,80 +1,23 @@
 import firstBankLogo from "../assets/firstbank.svg";
-import searchIcon from "../assets/search.svg";
-import Button from "../components/ui/Button";
+import WebMenu from "./WebMenu";
+import { IoMdMenu } from "react-icons/io";
 
 const NavBar = () => {
   return (
     <>
       <div className="h-[61px] bg-primary"></div>
 
-      <div className="w-full border-b-2 border-[#0000003B] bg-[#FFFFFF] flex px-[100px] h-[100px] place-items-center">
-        <div className="w-[105px] mr-[110px]">
+      <div className="md:hidden flex justify-between w-full border-b-2 border-[#0000003B] bg-red-100 px-[20px] h-[60px] place-items-center">
+        <div className="w-[100px] mr-[10px]">
           <img className="w-full" src={firstBankLogo} alt="firstbank logo" />
         </div>
-        <ul className="flex w-[30%] h-fit justify-between mr-[298px] font-medium text-primary">
-          <li>
-            <button
-              onClick={() => {
-                const homeRef = document.querySelector("#hero");
-                homeRef?.scrollIntoView({ behavior: "smooth" });
-              }}
-            >
-              Home
-            </button>
-          </li>
-          <li>
-            <button
-              onClick={() => {
-                const aboutRef = document.querySelector("#aboutUs");
-                aboutRef?.scrollIntoView({ behavior: "smooth" });
-              }}
-            >
-              About Us
-            </button>
-          </li>
-          <li>
-            <button
-              onClick={() => {
-                const trackRef = document.querySelector("#tracks");
-                trackRef.scrollIntoView({ behavior: "smooth" });
-              }}
-            >
-              Tracks
-            </button>
-          </li>
-          <li>
-            <a href="#gallery">Gallery</a>
-          </li>
-          <li>
-            <button
-              onClick={() => {
-                const faqRef = document.querySelector("#faq");
-                faqRef.scrollIntoView({ behavior: "smooth" });
-              }}
-            >
-              FAQs
-            </button>
-          </li>
-        </ul>
 
-        <div className="flex">
-          <div className="flex border-[1px] border-[#002855B2] rounded-[20px] w-[234px] h-[40px] px-2 mr-[43px]">
-            <div className="w-[26px] pt-[10px] pr-2">
-              <img className="w-full" src={searchIcon} alt="search icon" />
-            </div>
-            <form action="/search" method="GET">
-              <input
-                className="h-full w-[90%] italic border-none leading-10 tracking-wider px-2"
-                type="search"
-                name="navbar_search"
-                placeholder="Search..."
-                id=""
-              />
-            </form>
-          </div>
-          <Button btnText={"Login"} type={"navBtn"} />
+        <div className=" h:scale-50">
+          <IoMdMenu size="2em" />
         </div>
       </div>
+
+      <WebMenu />
     </>
   );
 };
---
+++ src/components/WebMenu.jsx
@@ -0,0 +1,80 @@
+import firstBankLogo from "../assets/firstbank.svg";
+import searchIcon from "../assets/search.svg";
+import Button from "../components/ui/Button";
+
+const WebMenu = () => {
+  return (
+    <div className="hidden md:block">
+      <div className="w-full border-b-2 border-[#0000003B] bg-[#FFFFFF] flex px-[100px] h-[100px] place-items-center">
+        <div className="w-[105px] mr-[110px]">
+          <img className="w-full" src={firstBankLogo} alt="firstbank logo" />
+        </div>
+        <ul className="flex w-[30%] h-fit justify-between mr-[298px] font-medium text-primary">
+          <li>
+            <button
+              onClick={() => {
+                const homeRef = document.querySelector("#hero");
+                homeRef?.scrollIntoView({ behavior: "smooth" });
+              }}
+            >
+              Home
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={() => {
+                const aboutRef = document.querySelector("#aboutUs");
+                aboutRef?.scrollIntoView({ behavior: "smooth" });
+              }}
+            >
+              About Us
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={() => {
+                const trackRef = document.querySelector("#tracks");
+                trackRef.scrollIntoView({ behavior: "smooth" });
+              }}
+            >
+              Tracks
+            </button>
+          </li>
+          <li>
+            <a href="#gallery">Gallery</a>
+          </li>
+          <li>
+            <button
+              onClick={() => {
+                const faqRef = document.querySelector("#faq");
+                faqRef.scrollIntoView({ behavior: "smooth" });
+              }}
+            >
+              FAQs
+            </button>
+          </li>
+        </ul>
+
+        <div className="flex">
+          <div className="flex border-[1px] border-[#002855B2] rounded-[20px] w-[234px] h-[40px] px-2 mr-[43px]">
+            <div className="w-[26px] pt-[10px] pr-2">
+              <img className="w-full" src={searchIcon} alt="search icon" />
+            </div>
+            <form action="/search" method="GET">
+              <input
+                className="h-full w-[90%] italic border-none leading-10 tracking-wider px-2"
+                type="search"
+                name="navbar_search"
+                placeholder="Search..."
+                id=""
+              />
+            </form>
+          </div>
+          <Button btnText={"Login"} type={"navBtn"} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WebMenu;
