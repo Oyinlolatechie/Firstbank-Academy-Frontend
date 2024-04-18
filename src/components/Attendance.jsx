9b6c3fb6d52b30a320f99d060f009dfa4aa51370
Oyinlolatechie <adepitanoyinlola4@gmail.com>
2024-04-18 13:31:11 +0100

    fix: attendabce section

--- src/components/Attendance.jsx
+++ src/components/Attendance.jsx
@@ -4,12 +4,12 @@ import playstore from "../assets/playstore.svg";
 const Attendance = () => {
   return (
     <>
-      <div className="bg-[#FFFFFF] w-screen px-4">
-        <h2 className="font-[700] text-[42px] md:text-[48px] text-primary text-center md:max-w-[398px] md:mx-auto leading-[46px] md:leading-[57.6px]">
+      <div className="bg-[#FFFFFF] w-screen">
+        <h2 className="font-[700] text-[42px] md:text-[48px] text-primary text-center w-fit md:max-w-[398px] md:mx-auto leading-[46px] md:leading-[57.6px]">
           Get the Academy Attendance App
         </h2>
 
-        <p className="font-[400] text-[16px] md:leading-[32px] md:w-[895px] mt-[32px] mb-[56px] md:mx-auto text-center">
+        <p className="font-[400] text-[16px] md:leading-[32px] w-fit md:w-[895px] mt-[32px] mb-[36px] md:mx-auto text-center">
           Efficient attendance tracking system with advanced biometric
           (Fingerprint) technology. This innovative system eliminates
           <br className="hidden md:block" /> the need for physical tickets,
@@ -18,11 +18,11 @@ const Attendance = () => {
           success and personal well-being.
         </p>
 
-        <div className="w-fit mx-auto">
+        <div className="w-[98%] mx-auto">
           <img src={phone} alt="mockup of three phone images" />
         </div>
 
-        <div className="md:flex w-[594px] mx-auto justify-between md:py-14">
+        <div className="md:flex md:w-[594px] md:mx-auto md:justify-between md:py-14">
           <button className="bg-[#F0BD2D] hover:bg-primary hover:text-[#F0BC26] my-3 md:my-0 font-[600] text-[14px] w-[307px] h-[40px] rounded-[20px] flex justify-center items-center mr-4">
             <span>
               <img className="pr-2" src={playstore} alt="" />
