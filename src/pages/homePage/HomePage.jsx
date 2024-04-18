55384413f2871e239be2e4f4f4ca2634f6884814
Oyinlolatechie <adepitanoyinlola4@gmail.com>
2024-04-18 13:11:46 +0100

    fix: debugging overflow

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -10,7 +10,7 @@ const HomePage = () => {
   return (
     <>
       <HeroBanner />
-      {/* <AboutUs /> */}
+      <AboutUs />
       {/* <TrackSection /> */}
       {/* <Attendance /> */}
       {/* <Housing /> */}
