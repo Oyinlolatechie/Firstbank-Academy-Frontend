689ec4dd49afbeab4f24b4fa5ffe5371018a6eb2
Oyinlolatechie <adepitanoyinlola4@gmail.com>
2024-04-18 09:28:59 +0100

    feat: implemented the attendance section responsiveness

--- src/components/Attendance.jsx
+++ src/components/Attendance.jsx
@@ -4,25 +4,26 @@ import playstore from "../assets/playstore.svg";
 const Attendance = () => {
   return (
     <>
-      <div className="bg-[#FFFFFF]">
-        <h2 className="font-[700] text-[48px] text-primary w-[398px] leading-[57.6px] mt-[53px] mx-auto">
+      <div className="bg-[#FFFFFF] px-4">
+        <h2 className="font-[700] text-[42px] md:text-[48px] text-primary text-center md:max-w-[398px] md:mx-auto leading-[46px] md:leading-[57.6px]">
           Get the Academy Attendance App
         </h2>
 
-        <p className="font-[400] text-[16px] leading-[32px] w-[895px] mt-[32px] mb-[56px] mx-auto text-center">
+        <p className="font-[400] text-[16px] md:leading-[32px] md:w-[895px] mt-[32px] mb-[56px] md:mx-auto text-center">
           Efficient attendance tracking system with advanced biometric
-          (Fingerprint) technology. This innovative system eliminates <br /> the
-          need for physical tickets, offering a seamless dining experience, and
-          empowers students <br /> to track attendance, fostering success and
-          personal well-being.
+          (Fingerprint) technology. This innovative system eliminates
+          <br className="hidden md:block" /> the need for physical tickets,
+          offering a seamless dining experience, and empowers students
+          <br className="hidden md:block" /> to track attendance, fostering
+          success and personal well-being.
         </p>
 
         <div className="w-fit mx-auto">
           <img src={phone} alt="mockup of three phone images" />
         </div>
 
-        <div className="flex w-[594px] mx-auto justify-between py-14">
-          <button className="bg-[#F0BD2D] hover:bg-primary hover:text-[#F0BC26] font-[600] text-[14px] w-[307px] h-[40px] rounded-[20px] flex justify-center items-center mr-4">
+        <div className="md:flex w-[594px] mx-auto justify-between  md:py-14">
+          <button className="bg-[#F0BD2D] hover:bg-primary hover:text-[#F0BC26] my-3 font-[600] text-[14px] w-[307px] h-[40px] rounded-[20px] flex justify-center items-center mr-4">
             <span>
               <img className="pr-2" src={playstore} alt="" />
             </span>
